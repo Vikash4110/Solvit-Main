3369caddbb28178447b7c29b08176b10a25b4cb7
s-jamwal <sahiljamwal2720@gmail.com>
2025-07-05 02:20:41 +0530

    firstCommit

---
+++ server/models/client-model.js
@@ -0,0 +1,132 @@
+const mongoose = require('mongoose')
+const bcrypt  = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const clientSchema = new mongoose.Schema({
+  
+    fullName : {
+        type : String,
+        required: [true, "Full Name is required"],
+        trim : true,
+        minlength : [3,"Full Name must atleast 3 characters"],
+        maxlength : [30,"Full Name can't exceed 30 characters"]
+    },
+    username : {
+        type : String,
+        index: true,
+        required: [true, "User Name is required"],
+        trim : true,
+        minlength : [3,"Full Name must atleast 3 characters"],
+        maxlength : [10,"Full Name can't exceed 30 characters"],
+        unique : true
+    },
+     password : {
+        type : String,
+        required : true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+      trim: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "Please enter a valid email address",
+      ],
+    },
+    phone: {
+      type: String,
+      unique: true,
+      match: [
+        /^\+?[1-9]\d{1,14}$/,
+        "Please enter a valid international phone number",
+      ],
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+    },
+    gender : {
+        type:String,
+        enum : ["Male","Female","Other","Prefer not to say"],
+        default : "Prefer not to say"
+    },
+    preferredLanguages : {
+        
+        type : [String],
+        enum:["Hindi","English"],
+        default :["Hindi","English"]
+        
+    },
+
+    bio: {
+      type: String,
+      trim: true,
+      maxlength: [500, "Bio must not exceed 500 characters"],
+    },
+
+    address: {
+      city: {
+        type: String,
+      },
+
+      area: {
+        type: String,
+      },
+
+      pincode: {
+        type: String,
+      },
+
+      coordinates: {
+        latitude: {
+          type: Number,
+        },
+        longitude: {
+          type: Number,
+        },
+      },
+    },
+    isBlocked : {
+        type : Boolean,
+        default : false
+    },
+    prefferedTopics : 
+        {
+            type:[String],
+            enum : ["Stress","mentalHealth"],
+            default : ["Stress","mentalHealth"]
+        },
+    lastLogin : {
+        type : Date
+    }
+        
+
+
+},{timestamps:true})
+
+
+clientSchema.pre("save", async function(next){
+    if(!this.isModified('password')) {return next()}
+    this.password = await bcrypt.hash(this.password ,10)
+    next()
+
+})
+
+clientSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password , this.password)
+}
+
+clientSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id : this.id
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+
+module.exports = mongoose.model("Client",clientSchema)
