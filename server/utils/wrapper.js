b010187bd4daa7c9bcf68f0f47b056e3dbafdf26
s-jamwal <sahiljamwal2720@gmail.com>
2025-07-05 01:32:32 +0530

    firstCommit

---
+++ server/utils/wrapper.js
@@ -0,0 +1,8 @@
+const wrapper = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next))
+      .catch((err) => next(err))
+  }
+}
+
+export {wrapper}
