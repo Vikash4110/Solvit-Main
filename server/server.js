37b7e36ef0846cc3fff4e59f43e01ecfd717e30e
s-jamwal <sahiljamwal2720@gmail.com>
2025-07-05 01:32:32 +0530

    firstCommit

--- server/server.js
+++ server/server.js
@@ -1,26 +1,40 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const connectDb = require('./utils/db');
+const connectDb = require('./database/connection.js');
+const cookieParser = require('cookie-parser')
 const app = express();
-const http = require('http');
-const server = http.createServer(app);
+
 
 
 app.use(cors({
     origin: ['http://localhost:5173',],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
   }));
 
 app.use(express.json());
+app.use(cookieParser())
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 const Port = process.env.PORT || 8000;
 
 connectDb()
   .then(() => {
-    server.listen(Port, () => {
+    app.listen(Port, () => {
       console.log(`Server is running on port ${Port}`);
     });
   })
