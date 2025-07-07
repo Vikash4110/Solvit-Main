e450fc86485d4b12ae199b88b7311882973fb73f
s-jamwal <sahiljamwal2720@gmail.com>
2025-07-07 12:54:14 +0530

    newCommit

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,14 +1,15 @@
-const mongoose = require('mongoose')
-const DB_Name= "solvit"
+import mongoose from 'mongoose';
+
+const DB_Name = "solvit";
+
 const connectDb = async () => {
-    try {
-        console.log(`${process.env.MONGODB_URI}/${DB_Name}`)
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`);
-        console.log('Database connected successfully');
-    } catch (error) {
-        console.error('Failed to connect to the database', error.message);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`);
+    console.log('Database connected successfully');
+  } catch (error) {
+    console.error('Failed to connect to the database', error.message);
+    process.exit(1);
+  }
 };
 
-module.exports = connectDb;
+export default connectDb;
