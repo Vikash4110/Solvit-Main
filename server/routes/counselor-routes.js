b6cf24c487e47312e396b8efe6e7ad451a53f8a3
Vikash4110 <139474262+Vikash4110@users.noreply.github.com>
2025-07-19 23:53:43 +0530

    main

--- server/routes/counselor-routes.js
+++ server/routes/counselor-routes.js
@@ -1,3 +1,40 @@
+// // File: src/routes/counselor-routes.js
+// import { Router } from "express";
+// import {
+//   loginCounselor,
+//   logoutCounselor,
+//   registerCounselor,
+//   sendOtpRegisterEmail,
+//   submitCounselorApplication,
+//   verifyOtpRegisterEmail,
+// } from "../controllers/counselor-controller.js";
+// import { verifyJWT } from "../middlewares/counselor-middleware.js";
+// import { upload } from "../middlewares/multer.middleware.js";
+
+// const counselorRouter = Router();
+
+// counselorRouter.route("/send-otp-register-email").post(sendOtpRegisterEmail);
+// counselorRouter
+//   .route("/verify-otp-register-email")
+//   .post(verifyOtpRegisterEmail);
+// counselorRouter
+//   .route("/register-counselor")
+//   .post(upload.single("profilePicture"), registerCounselor);
+// counselorRouter.route("/login-counselor").post(loginCounselor);
+// counselorRouter.route("/submit-application").post(
+//   verifyJWT,
+//   upload.fields([
+//     { name: "resume", maxCount: 1 },
+//     { name: "degreeCertificate", maxCount: 1 },
+//     { name: "licenseCertificate", maxCount: 1 },
+//     { name: "governmentId", maxCount: 1 },
+//   ]),
+//   submitCounselorApplication
+// );
+// counselorRouter.route("/logout-counselor").post(verifyJWT, logoutCounselor);
+
+// export { counselorRouter };
+
 // File: src/routes/counselor-routes.js
 import { Router } from "express";
 import {
@@ -8,7 +45,7 @@ import {
   submitCounselorApplication,
   verifyOtpRegisterEmail,
 } from "../controllers/counselor-controller.js";
-import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { verifyJWT } from "../middlewares/counselor-middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 const counselorRouter = Router();
@@ -26,8 +63,8 @@ counselorRouter.route("/submit-application").post(
   upload.fields([
     { name: "resume", maxCount: 1 },
     { name: "degreeCertificate", maxCount: 1 },
-    { name: "licenseCertificate", maxCount: 1 },
     { name: "governmentId", maxCount: 1 },
+    { name: "licenseCertificate", maxCount: 1 },
   ]),
   submitCounselorApplication
 );
