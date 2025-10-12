802bec3fc66736f49851009b3ccff30dbd6b4159
s-jamwal <sahiljamwal2720@gmail.com>
2025-10-12 22:04:07 +0530

    deployErrorFix3

--- client/src/components/client/ClientDashboard/ClientDashboardPayments.jsx
+++ client/src/components/client/ClientDashboard/ClientDashboardPayments.jsx
@@ -1,7 +1,7 @@
-export const ClientDashboardPayments = () => {
-  return (
-    <>
-      <h1>Payments</h1>
-    </>
+const ClientDashboardPayments = () => {
+  return (  
+      <h1>Payments</h1>  
   );
 };
+
+export { ClientDashboardPayments };
