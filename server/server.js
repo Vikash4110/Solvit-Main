654b6667977ec685c85fbf472c26b1fc8b5348d0
VikashXacco18 <vikash@xacco.co>
2025-11-18 00:27:22 +0530

    main

--- server/server.js
+++ server/server.js
@@ -81,7 +81,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Handle preflight requests explicitly
-app.options('*', cors(corsOptions));
+// app.options('*', cors(corsOptions));
 
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
