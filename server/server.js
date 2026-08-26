84cc7065ff012e4d612b61c71179da75888d1617
Dibyo Banerjee <dibyobanerjee005@gmail.com>
2026-08-26 19:46:16 +0530

    fix: lower body-parser payload limit from 50mb to 1mb

--- server/server.js
+++ server/server.js
@@ -72,8 +72,8 @@ app.use((req, res, next) => {
   }
 });
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ extended: true, limit: '1mb' }));
 app.use(express.static('public'));
 app.use(cookieParser());
 app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) } }));
